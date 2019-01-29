a219c18a4756d86df19f22dadba995ad21454688
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-01-29 18:32:49 +0000

    Adding linux netcdf libs

--- ExternalLibs/MakeLibraries.sh
+++ ExternalLibs/MakeLibraries.sh
@@ -6,8 +6,10 @@ base_dir=Base
 datetime_dir=datetime-fortran
 csv_dir=fortran-csv-module
 fox_dir=fox
-
 zlib_dir=zlib-1.2.11
+hdf5_dir=HDF5_1.8.17_x64/Linux
+netcdfc_dir=Netcdf_4.4.1.1/Linux
+netcdff_dir=netcdf-fortran-4.4.4
 
 cd $base_dir
 ./MakeLibraries.sh
@@ -36,6 +38,17 @@ cd $hdf5_dir
 make install
 cd ..
 
+cd $netcdfc_dir
+mkdir build
+./configure --prefix=$curr_dir/$netcdfc_dir/build
+make install
+cd ..
+
+cd $netcdff_dir
+mkdir build
+./configure --prefix=$curr_dir/$netcdff_dir/build
+make install
+cd ..
 
 echo ------------------------------------
 echo All done, have a nice day
