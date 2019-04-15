d2de4416f0ca88a09a5dd0a97ae2af40510f488d
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-04-15 15:52:59 +0100

    Update MakeLibraries.sh

--- ExternalLibs/MakeLibraries.sh
+++ ExternalLibs/MakeLibraries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curr_dir=$PWD
+root_libs_dir=$PWD
 
 base_dir=Base
 datetime_dir=datetime-fortran
@@ -17,61 +17,61 @@ mohid_dir=MOHID
 
 cd $base_dir
 ./MakeLibraries.sh
-cd $curr_dir
+cd $root_libs_dir
 
 cd $datetime_dir
 ./MakeLibraries.sh
-cd $curr_dir
+cd $root_libs_dir
 
 cd $csv_dir
 ./MakeLibraries.sh
-cd $curr_dir
+cd $root_libs_dir
 
 cd $fox_dir
 ./MakeLibraries.sh
-cd $curr_dir
+cd $root_libs_dir
 
 cd $zlib_dir
 mkdir build
-./configure --prefix=$curr_dir/$zlib_dir/build  || exit
+./configure --prefix=$root_libs_dir/$zlib_dir/build || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $hdf5_dir
-./configure --with-zlib=$curr_dir/$zlib_dir/build --enable-fortran --enable-fortran2003 --disable-shared  || exit
+./configure --with-zlib=$root_libs_dir/$zlib_dir/build --enable-fortran --enable-fortran2003 --disable-shared || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $netcdfc_dir
 mkdir build
 export CPPFLAGS="-I$hdf5_dir/hdf5/include -I$zlib_dir/build/include" 
 export LDFLAGS="-L$hdf5_dir/hdf5/lib -L$zlib_dir/build/lib"  
-./configure --enable-netcdf-4 --prefix=$pwd/build  || exit
+./configure --enable-netcdf-4 --prefix=$pwd/build || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $netcdff_dir
 mkdir build
-./configure --prefix=$curr_dir/$netcdff_dir/build  || exit
+./configure --prefix=$root_libs_dir/$netcdff_dir/build || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $proj4C_dir
 mkdir build
-./configure --prefix=$curr_dir/$proj4C_dir/build  || exit
+./configure --prefix=$root_libs_dir/$proj4C_dir/build || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $proj4F_dir
 mkdir build
 ./bootstrap
-./configure --with-proj4=$curr_dir/$proj4C_dir/build --prefix=$curr_dir/$proj4F_dir/build CFLAGS=f2cFortran  || exit
+./configure --with-proj4=$root_libs_dir/$proj4C_dir/build --prefix=$root_libs_dir/$proj4F_dir/build CFLAGS=f2cFortran || exit
 make install || exit
-cd $curr_dir
+cd $root_libs_dir
 
 cd $mohid_dir
 ./MakeLibraries.sh
-cd $curr_dir
+cd $root_libs_dir
 
 
 echo ------------------------------------
