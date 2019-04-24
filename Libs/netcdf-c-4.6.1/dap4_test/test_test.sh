efb66d9bcf13f440098c36e213857170d0ef580a
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-04-24 16:25:01 +0100

    Cleaning external libs

--- Libs/netcdf-c-4.6.1/dap4_test/test_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if test "x$srcdir" = "x" ; then srcdir=`dirname $0`; fi; export srcdir
-
-rm -f log.log
-sh -x ${srcdir}/test_parse.sh >& ./parse.log >& log.log
-cat log.log /dev/tty
-
-exit 0
