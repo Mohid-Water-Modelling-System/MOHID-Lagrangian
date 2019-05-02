d9587baf7a2d9a7aa0c74a95f17993cc0f608f84
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-05-02 14:45:11 +0100

    Update MOHIDLagrangianPreProcessor.py

--- src/MOHIDLagrangianPreProcessor/MOHIDLagrangianPreProcessor.py
+++ src/MOHIDLagrangianPreProcessor/MOHIDLagrangianPreProcessor.py
@@ -51,37 +51,25 @@ import os_dir
 def run():
     
     #cmd line argument parsing---------------------------
-    argParser = argparse.ArgumentParser(description='Indexes MOHID outputs in xmdf files. Use -h for help.')
-    argParser.add_argument("-i", "--input", dest="datadir",
+    argParser = argparse.ArgumentParser(description='Indexes input files for MOHID Lagrangian to parse. Use -h for help.')
+    argParser.add_argument("-case", "--caseXML", dest="caseXML",
+                    help=".xml file with the case definition for the MOHID Lagrangian run", metavar=".xml")
+    argParser.add_argument("-i", "--input", dest="dataDir",
                     help="input directory containing .hdf5 files or subdirectories with them", metavar="dir")
-    argParser.add_argument("-g", "--glue", dest="glueFiles", default=False,
-                    help="option to atempt to produce a master indexer, that 'glues' all of the files")
-    argParser.add_argument("-fd", "--firstdate", dest="firstDate", default='',
-                    help="option to control the first date for the master indexer, format as 2000-08-19 01:01:37")
-    argParser.add_argument("-ld", "--lastdate", dest="lastDate", default='',
-                    help="option to control the last date for the master indexer, format as 2000-08-19 01:01:37")
+    
     args = argParser.parse_args()
 
-    datadir = getattr(args,'datadir')
-    if datadir == None: #reverting to the test files
+    dataDir = getattr(args,'dataDir')
+    if dataDir == None: #reverting to the default
         basepath = os.path.dirname(__file__)
-        datadir = os.path.abspath(os.path.join(basepath, "..", "TestFiles"))        
-    glueFiles = getattr(args,'glueFiles')
-    if glueFiles != False:
-        glueFiles = True
-    firstDate = str(getattr(args,'firstDate'))
-    lastDate = str(getattr(args,'lastDate'))
+        dataDir = os.path.abspath(os.path.join(basepath, "inputData"))
+    caseXML = getattr(args,'caseXML')
     
     #datadir="C:\Users\RBC_workhorse\Documents\GitHub\MOHID_python_tools\ConvertCSV2HDF5\testFiles"
-    #glueFiles = True
-        
-    print('-> Main directory is', datadir)
-    if glueFiles:
-        print('-> Attempting to glue files')
-    if firstDate != '':
-        print('-> First date to be indexed is', firstDate)
-    if lastDate != '':
-        print('-> First date to be indexed is', lastDate)
+    
+    print('-> Case definition file is ', caseXML)
+    print('-> Data files directory is', dataDir)
+    
     #------------------------------------------------------    
     
             
