ba3ca233b7f901dbc83361ae3493d848925b7ddd
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-05-10 12:25:37 +0100

    Update RunCase.bat

--- RUN_Cases/Real_vel_test/RunCase.bat
+++ RUN_Cases/Real_vel_test/RunCase.bat
@@ -11,6 +11,9 @@ rem "executables" are renamed and called from their directory
 set tools=../../build/bin/RELEASE
 set mohidlagrangian="%tools%/MOHIDLagrangian.exe"
 
+set preprocessorDir=../../src/MOHIDLagrangianPreProcessor
+set PreProcessor="%preprocessorDir%/MOHIDLagrangianPreProcessor.py"
+
 rem "dirout" is created to store results or it is cleaned if it already exists
 if exist %dirout% del /Q %dirout%\*.*
 if not exist %dirout% mkdir %dirout%
@@ -19,6 +22,8 @@ copy %name%_Def.xml %dirout%
 ren %dirout%\%name%_Def.xml %name%.xml
 
 rem CODES are executed according the selected parameters of execution in this case
+
+python %PreProcessor% -i %dirout%/%name%.xml -o %dirout%
 
 %mohidlagrangian% -i %dirout%/%name%.xml -o %dirout%
 if not "%ERRORLEVEL%" == "0" goto fail
