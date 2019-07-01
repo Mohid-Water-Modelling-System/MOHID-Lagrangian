0c41dc8c047769b300e4387ca8e339a5d97601fc
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-07-01 17:33:21 +0100

    Adding copy method to state vector class

--- src/MOHIDLagrangianLib/stateVector.f90
+++ src/MOHIDLagrangianLib/stateVector.f90
@@ -41,12 +41,35 @@
         integer :: idx
     contains
     procedure :: toTracers
+    procedure :: copyState
     procedure :: finalize => cleanState
     end type stateVector_class
 
     public :: stateVector_class
 
     contains
+    
+    !---------------------------------------------------------------------------
+    !> @author Ricardo Birjukovs Canelas - MARETEC
+    !> @brief
+    !> Copies a State Vector to another.
+    !---------------------------------------------------------------------------
+    subroutine copyState(self, newsv)
+    class(stateVector_class), intent(in) :: self
+    type(stateVector_class), intent(out) :: newsv
+    
+    newsv%ttype = self%ttype
+    allocate(newsv%state(size(self%state,1), size(self%state,2)))
+    newsv%state = self%state
+    allocate(newsv%landMask(size(self%landMask)))
+    newsv%landMask = self%landMask
+    allocate(newsv%landIntMask(size(self%landIntMask)))
+    newsv%landIntMask = self%landIntMask
+    allocate(newsv%active(size(self%active)))
+    newsv%active = self%active
+    !maybe no need to copy source, id and tracer pointer
+    
+    end subroutine copyState
 
     !---------------------------------------------------------------------------
     !> @author Ricardo Birjukovs Canelas - MARETEC
