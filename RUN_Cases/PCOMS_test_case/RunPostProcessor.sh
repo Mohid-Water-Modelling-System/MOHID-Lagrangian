d6ad83046cb74942604dff3926cd9e59bc628b34
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-09-27 16:44:25 +0100

    update case files

---
+++ RUN_Cases/PCOMS_test_case/RunPostProcessor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+
+# "name" and "dirout" are named according to the testcase
+name=PCOMS_test_case
+dirout=${name}_out
+
+# "executables" are renamed and called from their directory
+
+postProcessorDir=../../src/MOHIDLagrangianPostProcessor
+postProcessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
+
+# CODES are executed according the selected parameters of execution in this testcase
+errcode=0
+
+python -W ignore $postProcessor -i ${name}.xml -o $dirout
+
+if [ $errcode -eq 0 ]; then
+  echo All done
+else
+  echo Execution aborted
+fi
+read -n1 -r -p "Press any key to continue..." key
+echo
