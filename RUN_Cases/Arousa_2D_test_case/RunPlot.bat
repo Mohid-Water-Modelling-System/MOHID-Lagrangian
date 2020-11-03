fc96a613452145072e3a6aafd637d7f562ed327b
DanielGaraboaPaz <angeldaniel.garaboa@usc.es>
2020-11-03 17:33:28 +0100

    Update docs and cases scripts

--- RUN_Cases/Arousa_2D_test_case/RunPlot.bat
+++ RUN_Cases/Arousa_2D_test_case/RunPlot.bat
@@ -5,7 +5,7 @@ call activate MOHID-Lagrangian
 
 rem "name" and "dirout" are named according to the case
 
-set name=Arousa2D_ship_case
+set name=Arousa2D
 set dirout=%name%_out
 
 rem "executables" are renamed and called from their directory
