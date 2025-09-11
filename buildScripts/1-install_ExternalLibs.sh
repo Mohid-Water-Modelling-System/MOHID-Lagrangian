21cef446be6a25499281defd4ce741655be50a23
shabani.mohsen <shabani.mohsen@outlook.com>
2025-09-11 15:22:32 +0200

    -Modify /buildScripts/1-instal..., to remove the inside folders

--- buildScripts/1-install_ExternalLibs.sh
+++ buildScripts/1-install_ExternalLibs.sh
@@ -2,6 +2,33 @@
 
 ### Make the changes to fit your setup ###
 
+#!/bin/bash
+
+#!/bin/bash
+
+# List of folders to remove
+folders=("MOHID-Lagrangian" "build" "src" "ExternalLibs")
+
+for folder in "${folders[@]}"; do
+    if [ -d "$folder" ]; then
+        echo "Removing folder: $folder"
+        
+        # Try with nohup first
+        nohup rm -rf "$folder" 2>/dev/null &
+        wait $!  # Wait for the nohup process to finish
+
+        # Check if folder still exists
+        if [ -d "$folder" ]; then
+            echo "nohup failed, trying normal rm..."
+            rm -rf "$folder"
+        fi
+    else
+        echo "Folder $folder does not exist, skipping."
+    fi
+done
+
+echo "Done."
+
 
 inteldir=/home/software/spack/opt/spack/linux-ubuntu24.04-sapphirerapids/gcc-13.3.0/intel-oneapi-compilers-2023.2.1-cz7grxfezyuoifpdwhgn6dcdbezbydez
 source $inteldir/setvars.sh
