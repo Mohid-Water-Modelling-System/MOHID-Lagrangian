e5733f303309119dbf8f960ddf59bb27a0cf1e19
shabani.mohsen <shabani.mohsen@outlook.com>
2026-01-20 10:21:26 +0100

    -Modify and add new 2-3D test cases in Mohsen_Cases folder

---
+++ RUN_Cases/Mohsen_Cases/Arousa_3D_Case_Detritus/RunCase_Compile_MOHIDLgrn.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+clear
+
+# Activate conda environment from shell. Minimum conda version required 4.6
+eval "$(conda shell.bash hook)"
+conda activate MOHID-Lagrangian
+
+# Increase stacksizes in Linux.
+ulimit -s hard
+export OMP_STACKSIZE=512M
+
+# "name" and "dirout" are named according to the testcase
+name=Arousa_3D_Def
+dirout=${name}_out
+data=data
+
+echo "Starting compile_MOHID-Lagrangian..."
+# Run the other script
+mohidlagrangian_buildScripts=../../../buildScripts
+${mohidlagrangian_buildScripts}/2-compile_Lagrangian.sh
+echo "Compile_MOHID-Lagrangian finished."
+
+# "executables" are renamed and called from their directory
+tools=../../../build/bin
+mohidlagrangian=${tools}/MOHIDLagrangian
+
+preprocessorDir=../../../src/MOHIDLagrangianPreProcessor
+mohidPreprocessor=${preprocessorDir}/MOHIDLagrangianPreProcessor.py
+
+postProcessorDir=../../../src/MOHIDLagrangianPostProcessor
+mohidPostprocessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
+
+# "dirout" is created to store results or it is cleaned if it already exists
+if [ -e $dirout ]; then
+  rm -f -r $dirout
+fi
+mkdir $dirout
+
+cp ${name}.xml $dirout/
+cp -r $data $dirout/
+
+# CODES are executed according the selected parameters of execution in this testcase
+
+python $mohidPreprocessor -i $dirout/${name}.xml -o $dirout
+
+errcode=0
+if [ $errcode -eq 0 ]; then
+  $mohidlagrangian -i $dirout/${name}.xml -o $dirout
+  errcode=$?
+fi
+
+#	python -W ignore $mohidPostprocessor -i ${name}.xml -o $dirout
+
+if [ $errcode -eq 0 ]; then
+  echo All done
+else
+  echo Execution aborted
+fi
+read -n1 -r -p "Press any key to continue..." key
+echo
